91ca3da137476801c97979c08854da72c32a4ef8
Elena Nelmes <enelmes@godaddy.com>
2025-07-11 17:36:08 +0300

    update App to show a box

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { ConsultantBox } from "./components/ConsultantBox";
+import { businessMockData } from "./mockData/business";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [customersIsLoading, setCustomersIsLoading] = useState(true);
+  const customersKeyword = "customers";
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ConsultantBox
+        className="box-customers"
+        isLoading={customersIsLoading}
+        loadingDuration={
+          businessMockData.find((item) => item.keyword === customersKeyword)
+            ?.loadingTime
+        }
+      >
+        <ConsultantBox.Title>
+          <>
+            Learning about your <b>{customersKeyword}</b>...
+          </>
+        </ConsultantBox.Title>
+        {customersIsLoading && (
+          <ConsultantBox.Description>
+            I'm digging into who your customers really are, so you can be the
+            most competitive in the market.
+          </ConsultantBox.Description>
+        )}
+      </ConsultantBox>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
