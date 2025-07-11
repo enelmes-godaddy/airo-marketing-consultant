2417783699ac4591d7ed7d2fa8470d91ca9c5807
Elena Nelmes <enelmes@godaddy.com>
2025-07-11 17:34:59 +0300

    add mock data

---
+++ src/mockData/business.tsx
@@ -0,0 +1,26 @@
+export const businessMockData = [
+  {
+    id: 'customers',
+    keyword: 'customers',
+    description: "I'm digging into who your customers really are, so you can be the most competitive in the market.",
+    loadingTime: 3000,
+  },
+  {
+    id: 'competitors',
+    keyword: 'competitors',
+    description: "I'm checking out who you're up against, giving you a clear picture of your competitors' strengths and weaknesses.",
+    loadingTime: 4500,
+  },
+  {
+    id: 'company',
+    keyword: 'company',
+    description: "I'm spotting what your business does best and where you can learn to grow.",
+    loadingTime: 6000,
+  },
+  {
+    id: 'climate',
+    keyword: 'climate',
+    description: "I'm eyeing the bigger picture—how outside trends and changes might impact your operation.",
+    loadingTime: 2500,
+  },
+];
---
+++ src/mockData/fourP.tsx
@@ -0,0 +1,26 @@
+export const fourPMockData = [
+  {
+    id: 'product',
+    keyword: 'product',
+    description: "I'm researching a product strategy to meet your customers' unique needs and desires.",
+    loadingTime: 3000,
+  },
+  {
+    id: 'pricing',
+    keyword: 'pricing',
+    description: "I'm building the right price plan for your product, considering factors like exclusivity and market demand.",
+    loadingTime: 4500,
+  },
+  {
+    id: 'places',
+    keyword: 'places',
+    description: "I'm finding the best locations and channels to showcase your product, so it reaches customers most likely to buy.",
+    loadingTime: 6000,
+  },
+  {
+    id: 'promote',
+    keyword: 'promote',
+    description: "I'm crafting a promotion strategy that communicates the value of your product to drive awareness and demand.",
+    loadingTime: 2500,
+  },
+];
